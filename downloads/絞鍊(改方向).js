c311a57d7a47052e0eea69274f6a85da11593b18
40823132 <40823132@gm.nfu.edu.tw>
2022-06-24 22:04:24 +0800

    w18任務1_wink影片和心得

---
+++ downloads/絞鍊(改方向).js
@@ -0,0 +1,34 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 15,
+  buttonFrameLength: 7,
+  buttonFrameOffset: 3,
+  frameStops: {
+    "3": [
+      { type: "gotoframe", rect: { x: 1589, y: 824, width: 60, height: 24 }, target: 4 },
+    ],
+    "10": [
+      { type: "gotoframe", rect: { x: 1572, y: 823, width: 60, height: 24 }, target: 3 },
+      { type: "gotoframe", rect: { x: 1649, y: 824, width: 60, height: 24 }, target: 11 },
+    ],
+    "32": [
+      { type: "gotoframe", rect: { x: 1386, y: 850, width: 60, height: 24 }, target: 10 },
+      { type: "gotoframe", rect: { x: 1465, y: 850, width: 60, height: 24 }, target: 33 },
+    ],
+    "69": [
+      { type: "gotoframe", rect: { x: 1392, y: 845, width: 60, height: 24 }, target: 32 },
+      { type: "gotoframe", rect: { x: 1464, y: 846, width: 60, height: 24 }, target: 70 },
+    ],
+    "106": [
+      { type: "gotoframe", rect: { x: 1387, y: 854, width: 60, height: 24 }, target: 69 },
+      { type: "gotoframe", rect: { x: 1456, y: 855, width: 60, height: 24 }, target: 107 },
+    ],
+    "143": [
+      { type: "gotoframe", rect: { x: 1676, y: 902, width: 60, height: 24 }, target: 144 },
+      { type: "gotoframe", rect: { x: 1608, y: 902, width: 60, height: 24 }, target: 106 },
+    ],
+    "180": [
+      { type: "gotoframe", rect: { x: 1450, y: 852, width: 60, height: 24 }, target: 143 },
+    ],
+  },
+};
